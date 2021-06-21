640c09de577c04db48a696ddfbe08ddcf31c3a93
CButtars <carson@buttars.org>
2021-06-21 02:11:34 -0600

    asdf

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import "@aws-amplify/ui-vue";
 import Amplify from "aws-amplify"
 import awsconfig from "./aws-exports"
 
+
 Amplify.configure(awsconfig);
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
