f01465b5dd801d2bb2ea55ebaa04c9537acef0eb
CButtars <carson@buttars.org>
2021-06-20 19:54:03 -0600

    Added amplify

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,67 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/vault">Vault</router-link> | 
-      <router-link to="/vault/1234">Vote</router-link>
-      <router-view/>
-    </div>
-    
+    <div v-if="authState !== 'signedin'">You are signed out.</div>
+      <amplify-authenticator username-alias="email">
+      <amplify-sign-up
+        slot="sign-up"
+        username-alias="email"
+        :form-fields.prop="formFields"
+      ></amplify-sign-up>
+      <amplify-sign-in slot="sign-in" username-alias="email"></amplify-sign-in>
+      <div v-if="user">
+        <div>Hello, {{user.username}}</div>
+
+
+        <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link> |
+          <router-link to="/vault">Vault</router-link> | 
+          <router-link to="/vault/1234">Vote</router-link> |
+          <router-view/>
+        </div>
+
+
+      </div>
+      <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
   </div>
 </template>
 
 <script>
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
 
 export default {
   name: 'app',
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
   data() {
     return {
-      user: { },
+      user: undefined,
+      authState: undefined,
+      unsubscribeAuth: undefined,
+      formFields: [
+        {
+          type: 'email',
+          label: 'Email',
+          placeholder: 'you@provider.com',
+          inputProps: { required: true, autocomplete: 'username' },
+        },
+        {
+          type: 'password',
+          label: 'Password',
+          placeholder: 'Custom password placeholder',
+          inputProps: { required: true, autocomplete: 'Aa1!Bb2@' },
+        }
+      ]
     }
   },
+  beforeDestroy() {
+    this.unsubscribeAuth();
+  }
 }
 </script>
 
